ebdc51f3917f6f34811b7724432ffc3ef2b394f0
 Dawei <100706617+dwhao84@users.noreply.github.com>
2024-09-05 18:25:01 +0800

    Update search_product.py

--- search_product.py
+++ search_product.py
@@ -1,7 +1,18 @@
-import  webbrowser
-# Example for using art_num as list, for searching product's article numbers.
-art_num = ["30362919", "60362913", "80362912"]
+import pandas as pd
+import webbrowser
 
+# 讀取 Excel 文件
+excel_file_name = 'Article_Number.xlsx'
+df = pd.read_excel(excel_file_name, sheet_name='Sheet1')  # 讀取指定的工作表
+
+# 選擇前兩行的數據，pandas 的 iloc 支援行範圍
+row_range = range(0, 3)  # 選擇前兩行
+row_data = df.iloc[row_range].values.tolist()  # 使用 iloc 來選擇行，並轉換為 list
+print(row_data)
+
+art_num = [item for sublist in row_data for item in (sublist if isinstance(sublist, list) else [sublist])]
+
+# Example for using art_num as list, for searching product's article numbers.
 # Open web browser fast by using For-Loop function.
 for i in art_num:
     url = f"https://www.ikea.com.tw/zh/search?q= { i }"
