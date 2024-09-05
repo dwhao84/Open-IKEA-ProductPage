49831931f78ece78f4d67ff56470043ab43e65e1
 Dawei <100706617+dwhao84@users.noreply.github.com>
2024-09-05 12:07:48 +0800

    Update search_product.py

--- search_product.py
+++ search_product.py
@@ -1,6 +1,8 @@
 import  webbrowser
+# Example for using art_num as list, for searching product's article numbers.
 art_num = ["30362919", "60362913", "80362912"]
 
+# Open web browser fast by using For-Loop function.
 for i in art_num:
     url = f"https://www.ikea.com.tw/zh/search?q= { i }"
     webbrowser.open(url)
